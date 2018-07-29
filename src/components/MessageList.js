4dea02adf4f5ad14983f25aa6af8e1afffcff566
Claire Ouellette <claireelisey@gmail.com>
2018-07-28 23:41:11 -0500

    Checkpoint 6 Send Messages: Completed

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -6,14 +6,19 @@ class MessageList extends Component {
         this.state = {
             messages: [],
             message: {
-                username: " ",
-                content: " ",
-                roomId: " ",
+                username: '',
+                content: '',
+                roomId: '',
                 sentAt: this.props.firebase.database.ServerValue.TIMESTAMP
-            }
+            },
+            newMessageContent: " "
         };
 
         this.messagesRef = this.props.firebase.database().ref('messages');
+        this.createMessage = this.createMessage.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+
     }
 
     componentDidMount () {
@@ -24,15 +29,38 @@ class MessageList extends Component {
         });
     }
 
-//    handleChange(e) {
-//        e.preventDefault();
-//        this.setState({
-//          username: this.props.currentUser.displayName,
-//          content: e.target.value,
-//          sentAt: this.props.firebase.database.ServerValue.TIMESTAMP,
-//          roomId: this.props.activeRoom.key
-//        });
-//    }
+    createMessage(newMessageContent) {
+        this.messagesRef.push(
+            {
+                content: newMessageContent,
+                username: this.props.currentUser.displayName,
+                sentAt: this.props.firebase.database.ServerValue.TIMESTAMP,
+                roomId: this.props.activeRoom
+            }
+        );
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        if (this.state.newMessageContent !== " ") {
+            this.createMessage (this.state.newMessageContent);
+            this.setState (
+                {
+                    newMessageContent: " ",
+                    username: " "
+                }
+            );
+        }
+    }
+
+    handleChange(e){
+        this.setState(
+            {
+                newMessageContent: e.target.value,
+                username: this.props.currentUser.displayName
+            }
+        );
+    }
 
     render () {
         return (
@@ -41,9 +69,18 @@ class MessageList extends Component {
                     { this.state.messages.map ( (message, index) => {
                         if (this.props.activeRoom && this.props.activeRoom === message.roomId) {
                             return <li key={index}>{message.username}: {message.content} {message.sentAt}</li>
-                      } 
+                        } else {
+                          return null
+                        }
                     })}
                 </ul>
+                <form onSubmit={ (e) => this.handleSubmit(e) }>
+                    <input type="text"
+                        value= { this.state.newMessageContent }
+                        placeholder= "Enter your message..."
+                        onChange= { this.handleChange } />
+                    <input type="submit" value="submit" />
+                </form>
             </section>
         );
     }
